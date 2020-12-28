a3df710d364d5766d9bc18057625a6998de32e3b
jbeckmann1 <jo.bec@t-online.de>
2020-12-28 12:17:40 +0100

    Adding a Sign Up Form and Post route

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log('hi there');
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+	res.send(`
+    <div>
+    <form method="POST">
+    <input name="email" placeholder="email" />
+    <input name="password" placeholder="password" />
+    <input name="passwordConfirmation" placeholder="Confirm password" />
+    <button> Sign Up</button>
+    </form>
+    `);
+});
+app.post('/', (req, res) => {
+	res.send('Account created!!!');
+});
+app.listen(3000, () => {
+	console.log('listening');
+});
