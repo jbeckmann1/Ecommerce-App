c4ebccd09dfdda5bcf64d3bdb8b67adc3eae9892
jbeckmann1 <jo.bec@t-online.de>
2020-12-29 10:42:29 +0100

    Finding Users by Id

--- repositories/users.js
+++ repositories/users.js
@@ -41,13 +41,16 @@ class UsersRepository {
 	randomId() {
 		return crypto.randomBytes(4).toString('hex');
 	}
+	async getOne(id) {
+		const records = await this.getAll();
+		return records.find((record) => record.id === id);
+	}
 }
 
 const test = async () => {
 	const repo = new UsersRepository('users.json');
-	await repo.create({ email: 'test@test.com', password: 'password' });
-	const users = await repo.getAll();
-	console.log(users);
+	const user = await repo.getOne('e15a3860');
+	console.log(user);
 };
 
 test();
