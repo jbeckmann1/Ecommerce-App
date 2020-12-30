42aa082153d43b6c3ac91c1be934589d897a29b7
jbeckmann1 <jo.bec@t-online.de>
2020-12-30 06:46:19 +0100

    Delete Function

--- repositories/users.js
+++ repositories/users.js
@@ -45,12 +45,17 @@ class UsersRepository {
 		const records = await this.getAll();
 		return records.find((record) => record.id === id);
 	}
+	async delete(id) {
+		const records = await this.getAll();
+		const filteredRecords = records.filter((record) => record.id !== id);
+		await this.writeAll(filteredRecords);
+	}
 }
 
 const test = async () => {
 	const repo = new UsersRepository('users.json');
-	const user = await repo.getOne('e15a3860');
-	console.log(user);
+
+	await repo.delete('e15a3860');
 };
 
 test();
