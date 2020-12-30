0bf4cf270d7138403e1bcea01d62dc1e2096da21
jbeckmann1 <jo.bec@t-online.de>
2020-12-30 07:10:26 +0100

    getOneBy Method

--- repositories/users.js
+++ repositories/users.js
@@ -60,12 +60,29 @@ class UsersRepository {
 		Object.assign(record, attrs);
 		await this.writeAll(records);
 	}
+	async getOneBy(filters) {
+		const records = await this.getAll();
+
+		for (let record of records) {
+			let found = true;
+			//looping through an object -> for in Loop(for every key)
+			for (let key in filters) {
+				if (record[key] !== filters[key]) {
+					found = false;
+				}
+			}
+			if (found) {
+				return record;
+			}
+		}
+	}
 }
 
 const test = async () => {
 	const repo = new UsersRepository('users.json');
 
-	await repo.update('81bfargraga3', { password: 'password' });
+	const user = await repo.getOneBy({ email: 'test@test.com', password: 'password3' });
+	console.log(user);
 };
 
 test();
