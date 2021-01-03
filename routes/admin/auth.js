f300badb26de05c2ce7d507a552b8a2cf20b6916
jbeckmann1 <jo.bec@t-online.de>
2021-01-03 16:05:00 +0100

    Implement Validation and Sanitization/Merge Custom Validation

--- routes/admin/auth.js
+++ routes/admin/auth.js
@@ -1,4 +1,6 @@
 const express = require('express');
+//Just the function we need
+const { check, validationResult } = require('express-validator');
 const usersRepo = require('../../repositories/users');
 const signupTemplate = require('../../views/admin/auth/signup');
 const signinTemplate = require('../../views/admin/auth/signin');
@@ -7,22 +9,41 @@ const router = express.Router();
 router.get('/signup', (req, res) => {
 	res.send(signupTemplate({ req }));
 });
-router.post('/signup', async (req, res) => {
-	const { email, password, passwordConfirmation } = req.body;
-	const existingUser = await usersRepo.getOneBy({ email });
-	if (existingUser) {
-		return res.send('Email in use');
+router.post(
+	'/signup',
+	//Sanitization and Validation with express validator
+	[
+		check('email').trim().normalizeEmail().isEmail().withMessage('Must be a valid email').custom(async (email) => {
+			const existingUser = await usersRepo.getOneBy({ email });
+			if (existingUser) {
+				throw new Error('Email in use');
+			}
+		}),
+		check('password')
+			.trim()
+			.isLength({ min: 4, max: 20 })
+			.withMessage('Password has to be between 4 and 20 Characters'),
+		check('passwordConfirmation')
+			.trim()
+			.isLength({ min: 4, max: 20 })
+			.withMessage('Password has to be between 4 and 20 Characters')
+			.custom((passwordConfirmation, { req }) => {
+				if (req.body.password !== passwordConfirmation) {
+					throw new Error('Password must match');
+				}
+			})
+	],
+	async (req, res) => {
+		const error = validationResult(req);
+		console.log(error);
+		const { email, password, passwordConfirmation } = req.body;
+		// Create a user in our user reepo to represent this person
+		const user = await usersRepo.create({ email, password });
+		//Store the id of that user inside the users cookie
+		req.session.userId = user.id; //req.session is an object Added by cookie session which can be configured
+		res.send('Account created!!!');
 	}
-	if (password !== passwordConfirmation) {
-		res.send('Password must match');
-	}
-	// Create a user in our user reepo to represent this person
-	const user = await usersRepo.create({ email, password });
-
-	//Store the id of that user inside the users cookie
-	req.session.userId = user.id; //req.session is an object Added by cookie session which can be configured
-	res.send('Account created!!!');
-});
+);
 
 router.get('/signout', (req, res) => {
 	req.session = null;
