0194632b2644680cbdfe05c1baff98faee4f73ba
jbeckmann1 <jo.bec@t-online.de>
2021-01-05 10:05:18 +0100

    Implement reusable repository class

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const usersRepo = require('./repositories/users.js');
 const cookieSession = require('cookie-session');
 const authRouter = require('./routes/admin/auth');
+const productsRouter = require('./routes/admin/products');
 const app = express();
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -15,6 +16,7 @@ app.use(
 );
 
 app.use(authRouter);
+app.use(productsRouter);
 
 app.listen(3000, () => {
 	console.log('listening');
--- repositories/users.js
+++ repositories/users.js
@@ -1,32 +1,11 @@
 const fs = require('fs');
 const crypto = require('crypto');
 const util = require('util');
+const Repository = require('./repository');
 const scrypt = util.promisify(crypto.scrypt);
-
-class UsersRepository {
+//Copy paste repository into the users repository
+class UsersRepository extends Repository {
 	//check if there is already a file/Create one
-	constructor(filename) {
-		if (!filename) {
-			throw new Error('Creating a repository requires a filename');
-		}
-		try {
-			this.filename = filename;
-			fs.accessSync(this.filename);
-		} catch (err) {
-			fs.writeFileSync(this.filename, '[]');
-		}
-	}
-	async getAll() {
-		// OPen the file calls this.filename
-		// Read its contents
-		// Parse the contents
-		//Return the parsed data
-		return JSON.parse(
-			await fs.promises.readFile(this.filename, {
-				encoding : 'utf8'
-			})
-		);
-	}
 	async create(attrs) {
 		attrs.id = this.randomId();
 
@@ -51,51 +30,6 @@ class UsersRepository {
 		const [ hashed, salt ] = saved.split('.');
 		const hashedSuppliedBuf = await scrypt(supplied, salt, 64);
 		return hashed === hashedSuppliedBuf.toString('hex');
-	}
-
-	async writeAll(records) {
-		//rewrite the file and formatting it
-		await fs.promises.writeFile(this.filename, JSON.stringify(records, null, 2));
-	}
-
-	randomId() {
-		return crypto.randomBytes(4).toString('hex');
-	}
-	async getOne(id) {
-		const records = await this.getAll();
-		return records.find((record) => record.id === id);
-	}
-	async delete(id) {
-		const records = await this.getAll();
-		const filteredRecords = records.filter((record) => record.id !== id);
-		await this.writeAll(filteredRecords);
-	}
-	async update(id, attrs) {
-		const records = await this.getAll();
-		const record = records.find((record) => record.id === id);
-
-		if (!record) {
-			throw new error(`Record with id ${id} not found`);
-		}
-		//Update the records with attrs
-		Object.assign(record, attrs);
-		await this.writeAll(records);
-	}
-	async getOneBy(filters) {
-		const records = await this.getAll();
-
-		for (let record of records) {
-			let found = true;
-			//looping through an object -> for in Loop(for every key)
-			for (let key in filters) {
-				if (record[key] !== filters[key]) {
-					found = false;
-				}
-			}
-			if (found) {
-				return record;
-			}
-		}
 	}
 }
 //export an instance and not just the class
