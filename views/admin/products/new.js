4fdae3098fe270d8f73aa8ccbecac39d104c7f97
jbeckmann1 <jo.bec@t-online.de>
2021-01-07 09:57:44 +0100

    Bugfix Middleware products post route

--- views/admin/products/new.js
+++ views/admin/products/new.js
@@ -1,11 +1,12 @@
 const layout = require('../layout');
 const { getError } = require('../../helpers');
+
 module.exports = ({ errors }) => {
 	return layout({
 		content : `
         <form method="POST"enctype="multipart/form-data">
-        <input placeholder="Title" name="title"/>
-        <input placeholder="Price" name="price"/>
+        <input placeholder="Title" name="title"/>${getError(errors, 'title')}
+        <input placeholder="Price" name="price"/>${getError(errors, 'price')}
         <input type="file" name="image"/>
         <button>Submit</button>
         </form>
